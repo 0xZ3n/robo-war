e2e051fcb5be939840d70769c16fe00ff2a07696
Devin Simpson <82175278+0xZ3n@users.noreply.github.com>
2022-08-08 14:36:37 -0400

    added functionality for fighting through array of enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,11 +7,19 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"]; console.log(enemyNames);
 var enemyHealth = 50;
 var enemyAttack = 12;
+//console.log("Array length of enemyNames is: " + enemyNames.length);
 
-var fight = function() {
+
+// for(var i = 0; i < enemyNames.length; i++) {
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] + " is at " + i + " index");
+// }
+
+var fight = function(enemyName) {
       // Alert players that they are starting the round
 
   window.alert("Welcome to Robot Gladiators!");
@@ -61,7 +69,12 @@ var fight = function() {
 
     } else {
         window.alert("You need to choose a valid option. Try again!");
+        fight();
     }
 };
 
-fight();
+//fight();
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
